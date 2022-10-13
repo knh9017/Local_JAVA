5d74d3e8b373921873e96070e594e4f5383fb186
Mad-Alchemist <105469951+Mad-Alchemist@users.noreply.github.com>
2022-10-13 11:27:24 +0900

    Update ODBC.java

---
+++ JCP_LOCAL/src/Server/ODBC.java
@@ -0,0 +1,58 @@
+package Server;
+import java.sql.*;
+import java.util.Scanner;
+
+public class ODBC {
+
+    static Connection conn;
+    static Statement stmt;
+
+    public static void sql_signin(String uid,String pwd)
+    {
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+
+            conn = DriverManager.getConnection(url, uid, pwd);
+            stmt = conn.createStatement();
+            System.out.println("Connection Success");
+            
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Failed to load driver");
+        }
+        catch(SQLException e){
+            System.out.println("Error : " + e);
+        }
+    
+    }
+
+    public static void sql_close()
+    {
+        if( conn != null ){
+            try{
+                stmt.close();
+                conn.close();
+            }
+            catch(SQLException e)
+            {
+                System.out.println("An error occured when closing.");
+            }
+        }
+    }
+
+    public static void DB_con() {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(conn == null){
+            System.out.print("ID : ");
+            String uid = sc.next();
+            System.out.print("Password : ");
+            String pwd = sc. next();
+            sql_signin(uid,pwd);
+            }
+
+        sc.close();
+    }
+}
